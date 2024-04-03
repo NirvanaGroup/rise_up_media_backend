77e63cfa22cfcdf553ffbe03d1cdb8d95015fbdf
nikerocker <nekhileshk@gmail.com>
2024-04-03 19:12:43 +0530

    minor fixes to run in local environment of nekhilesh

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { PdfsModule } from './pdfs/pdfs.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/riseUpMedia'),
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017/riseUpMedia'),
     EventsModule,
     PdfsModule,
   ],
