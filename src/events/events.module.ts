d89d71520bb2980b5ed1354e3d9f3e889512b3e8
nikerocker <nekhileshk@gmail.com>
2024-04-14 10:39:33 +0530

    udpated the paths for the schemas folder

--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { Event, EventSchema } from '../../src/schemas/Event.schema';
+import { Event, EventSchema } from '../schemas/Event.schema';
 import { EventsService } from './events.service';
 import { EventsController } from './events.controller';
-import { Pdf, PdfSchema } from '../../src/schemas/pdf.schema';
+import { Pdf, PdfSchema } from '../schemas/pdf.schema';
 
 @Module({
   imports: [
