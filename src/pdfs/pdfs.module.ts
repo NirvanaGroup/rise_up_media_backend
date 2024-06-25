6226d311d46e1536bf09f2a939f27afc1f11791a
nikerocker <nekhileshk@gmail.com>
2024-06-25 21:50:45 +0530

    relative path error fix

--- src/pdfs/pdfs.module.ts
+++ src/pdfs/pdfs.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { Pdf, PdfSchema } from 'src/schemas/pdf.schema';
+import { Pdf, PdfSchema } from '../schemas/pdf.schema';
 import { PdfsController } from './pdfs.controller';
 import { PdfsService } from './pdfs.service';
 
